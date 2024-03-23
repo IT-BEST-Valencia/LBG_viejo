9f987901df2077ca7769c47b2de49c5318e2db39
Parri0627 <122632877+Parri0627@users.noreply.github.com>
2024-03-23 18:31:11 +0100

    Inicialización viejo LBG

---
+++ assets/js/carousel.js
@@ -0,0 +1,34 @@
+var timeout;
+var slideIndex = 1;
+
+function showDivs(n) {
+    var i;
+    var x = document.getElementsByClassName("mySlides");
+    if (n > x.length) {slideIndex = 1} 
+    if (n < 1) {slideIndex = x.length} ;
+    for (i = 0; i < x.length; i++) {
+        x[i].style.display = "none"; 
+    }
+    x[slideIndex-1].style.display = "block"; 
+}
+    
+function plusDivs(n) {
+    showDivs(slideIndex += n);
+    clearTimeout(timeout);
+    timeout = setTimeout(carousel, 10000);
+}
+       
+
+                        
+function carousel() {
+    var i;
+    var x = document.getElementsByClassName("mySlides");
+    for (i = 0; i < x.length; i++) {
+        x[i].style.display = "none";
+    }
+    slideIndex++;
+    if (slideIndex > x.length) {slideIndex = 1} 
+    x[slideIndex-1].style.display = "block";
+    clearTimeout(timeout);
+    timeout = setTimeout(carousel, 10000);
+}
